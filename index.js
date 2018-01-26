a78a6c6cceb8a46a203c56325db5a448937ed7d4
Mingyi Zhang <zhangmingyi@kedacom.com>
2018-01-26 10:26:48 +0800

    init

---
+++ index.js
@@ -0,0 +1,40 @@
+const Plugin = require('node-plugin').Plugin
+const process = require('process')
+var events = require('events');
+var EventEmitter = new events.EventEmitter();
+
+class WebStreamer extends EventEmitter {
+    constructor( name ) {
+		this.name = name;
+		this.plugin_ = new Plugin(name)		
+    }
+
+    initialize(options) {
+		this.plugin_.initialize(options,this.on_notify_);
+	}
+
+	on_notify_(buf){
+		console.log(buf.toString());
+	}
+
+	terminate(done){
+		this.plugin_.release(done);
+	}
+
+	call_(buf,cb){
+		this.plugin_.call(buf,cb);
+	}
+
+}
+
+
+
+//example = path.join(__dirname,'/bin/calc'+_EXT)
+module.exports = {
+	WebStreamer  : WebStreamer
+}
+
+
+
+//const Plugin = require('node-plugin').Plugin
+
