bf08c0ab48640dd7aff20c517c01543e02cb4ce9
ganchao <ganchao@kedacom.com>
2018-02-02 17:18:44 +0800

    ready for use

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const Plugin = require('node-plugin').Plugin
-const process = require('process')
+const Plugin = require('node-plugin').Plugin;
+const process = require('process');
 var events = require('events');
 var EventEmitter = new events.EventEmitter();
+const liveStreamBufGenerator = require('./lib/livestream');
 
 class WebStreamer extends EventEmitter {
     constructor( name ) {
@@ -12,18 +13,19 @@ class WebStreamer extends EventEmitter {
 
     initialize(options) {
 		this.plugin_.initialize(options,this.on_notify_);
+        this.plugin_.on('notify', this.on_notify_);
 	}
 
 	on_notify_(buf){
 		console.log(buf.toString());
 	}
 
-	terminate(done){
-		this.plugin_.release(done);
+	terminate(){
+		return this.plugin_.terminate();
 	}
 
 	call_(buf){
-		this.plugin_.call(buf,cb);
+		return this.plugin_.call(buf);
 	}
 
 }
@@ -32,7 +34,8 @@ class WebStreamer extends EventEmitter {
 
 //example = path.join(__dirname,'/bin/calc'+_EXT)
 module.exports = {
-	WebStreamer  : WebStreamer
+	WebStreamer  : WebStreamer,
+    liveStreamBufGenerator,
 };
 
 
