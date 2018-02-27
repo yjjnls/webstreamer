f33aa1d1bab59cf8b33b4a74d2acff9dc6a83899
ganchao <ganchao@kedacom.com>
2018-02-27 14:57:33 +0800

    add log

--- index.js
+++ index.js
@@ -25,23 +25,27 @@ class LiveStream {
 	}
 
 	async createLiveStream(streamId, endpointId, endpointType, rtspurl, videoCodec, audioCodec) {
+	    console.log(`=======createLiveStream=======`);
 		let buf = liveStreamBufGenerator.generateLiveStreamCreateMsgBuf(streamId, endpointId, endpointType, rtspurl, videoCodec, audioCodec);
 		let res = await webstreamer.call_(buf);
 	}
 
 	async destroyLiveStream(streamId) {
+        console.log(`=======destroyLiveStream=======`);
 		let buf = liveStreamBufGenerator.generateLiveStreamDestroyMsgBuf(streamId);
 		let res = await webstreamer.call_(buf);
 		return res;
 	}
 
 	async liveStreamAddEndpoint(streamId, endpointId) {
+        console.log(`=======liveStreamAddEndpoint=======`);
 		let buf = liveStreamBufGenerator.generateLiveStreamAddEndpointMsgBuf(streamId, endpointId);
 		let res = await webstreamer.call_(buf);
 		return res;
 	}
 
 	async liveStreamRemoveEndpoint(streamId, endpointId) {
+        console.log(`=======liveStreamRemoveEndpoint=======`);
 		let buf = liveStreamBufGenerator.generateLiveStreamRemoveEndpointMsgBuf(streamId, endpointId);
 		let res = await webstreamer.call_(buf);
 		return res;
