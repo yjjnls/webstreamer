c22aed884b8cb39627f1fd90846b0c4b927a0cb6
mingyi <mingyi.z@outlook.com>
2018-03-23 15:40:05 +0800

    add processor and rtsp test server

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
 
-const WebStreamer = require('./lib/webstreamer').WebStreamer;
+const WebStreamer  = require('./lib/webstreamer').WebStreamer;
+var RTSPTestServerBase = require('./lib/rtsptestserver').RTSPTestServer;
+var webstreamer_=null
 
+function Initialize(option){
+
+	if( webstreamer_ ){
+		return;
+	}
+
+    webstreamer_ = new WebStreamer()
+
+}
+
+function Terminate(){
+	if( !webstreamer_ ){
+		return;
+	}
+	return webstreamer_.terminate();
+}
+
+
+class RTSPTestServer extends RTSPTestServerBase  {
+	constructor(name) {
+        super(webstreamer_,name);
+    }
+}
 
 module.exports = {
 	WebStreamer : WebStreamer,
+
+	Initialize : Initialize,
+	Terminate  : Terminate,
+	RTSPTestServer : RTSPTestServer
 };
