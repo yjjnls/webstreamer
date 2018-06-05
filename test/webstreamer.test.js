3ff7bbd6e5ae82c1f84bc6368e00d45e4d05482b
yuanjunjie <yanjunjie@kedacom.com>
2018-06-05 14:45:54 +0800

    linux build

--- test/webstreamer.test.js
+++ test/webstreamer.test.js
@@ -7,13 +7,13 @@ describe('WebStreamer', function () {
     describe('#interface', function () {
         before(async () => {
             try {
-                await plugin.Initialize({
+            await plugin.Initialize({
                     rtsp_server: {
                         port: 555
                     }
                 });
-            } catch (err) {
-                throw new Error(err.toString());
+        } catch (err) {
+                throw new Error(err.message);
             }
         });
 
